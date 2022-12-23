855d906180d297e43e4ccb1ff9b01819a19dc5ad
Melissa Barrios <melissa.blecona@gmail.com>
2022-12-23 10:53:05 -0600

    proyecto raiz- con practicas y ejemplos

---
+++ raiz/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AppRouter from "./routers/AppRouter";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const App = () => {
+  return <AppRouter />;
+};
+
+export default App;
